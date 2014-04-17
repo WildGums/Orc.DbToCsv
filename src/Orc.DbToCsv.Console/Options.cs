bc44f3a4a4fed9fef4da9f98539a9c7cbfae25da
auz34 <andrewzavgorodniy@gmail.com>
2014-04-17 08:05:37 +0300

    Making project parameter optional

--- src/Orc.DbToCsv.Console/Options.cs
+++ src/Orc.DbToCsv.Console/Options.cs
@@ -4,7 +4,7 @@
 
     public class Options
     {
-        [Option('p', "project", Required = true, HelpText = "Path to the xml file defining import project")]
+        [Option('p', "project", Required = false, HelpText = "Path to the xml file defining import project")]
         public string Project { get; set; }
 
         [Option('o', "output", Required = false, HelpText = "Output folder path")]
--- src/Orc.DbToCsv.Console/Program.cs
+++ src/Orc.DbToCsv.Console/Program.cs
@@ -21,13 +21,48 @@
                     Environment.Exit(1);
                 });
 
-            var project = Project.Load(options.Project);
+            Project project = null;
+            if (!string.IsNullOrEmpty(options.Project))
+            {
+                project = Project.Load(options.Project);
+            }
+            else
+            {
+                project = TryGetProjectAutomatically();
+            }
+
+            if (project == null)
+            {
+                Console.WriteLine("Unable to locate the project to process.");
+                Environment.Exit(1);
+            }
+            
 
             string outputFolder = string.IsNullOrEmpty(options.OutputFolder) ?
                 Directory.GetCurrentDirectory() :
                 options.OutputFolder;
 
             Importer.ProcessProject(project, outputFolder, new ConsoleWriter());
+        }
+
+        private static Project TryGetProjectAutomatically()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            FileInfo[] candidates = directoryInfo.GetFiles("*.iprj");
+            foreach (FileInfo candidate in candidates)
+            {
+                try
+                {
+                    var project = Project.Load(candidate.FullName);
+                    return project;
+                }
+                catch 
+                {
+                    continue;
+                }
+            }
+
+            return null;
         }
     }
 }

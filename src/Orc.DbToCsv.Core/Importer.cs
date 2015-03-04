350110f64bdc387bec002c839354bbd224f24f30
Orcomp <Orcomp@server>
2015-03-04 14:45:22 +1100

    Add more logging

--- src/Orc.DbToCsv.Core/Importer.cs
+++ src/Orc.DbToCsv.Core/Importer.cs
@@ -49,10 +49,10 @@
 
             try
             {
-                List<Tuple<string, string>> schema = GetTableSchema(sqlConnection, tableName);
+                List<Tuple<string, string>> schema = GetTableSchema(sqlConnection, tableName, logWriter);
                 if (schema.Count == 0)
                 {
-                    logWriter.WriteLine(string.Format("No columns has found in {0} table to import into csv format", tableName));
+                    logWriter.WriteLine(string.Format("No columns was found in the '{0}' table to export into a csv file.", tableName));
                     return;
                 }
 
@@ -93,7 +93,7 @@
                 }
 
                 string okResult = string.Format(
-                "{0} records of {1} table succesfully imported to {2}.",
+                "'{0}' records of '{1}' table succesfully exported to {2}.",
                 records,
                 tableName,
                 fileName);
@@ -102,7 +102,7 @@
             }
             catch (Exception ex)
             {
-                logWriter.WriteLine(string.Format("{0} import failed because of exception: {1}", tableName, ex.Message));
+                logWriter.WriteLine(string.Format("{0} export failed because of exception: {1}", tableName, ex.Message));
             }
         }
 
@@ -115,7 +115,7 @@
 
         }
 
-        private static List<Tuple<string, string>> GetTableSchema(SqlConnection sqlConnection, string tableName)
+        private static List<Tuple<string, string>> GetTableSchema(SqlConnection sqlConnection, string tableName, ILogWriter logWriter)
         {
             List<Tuple<string, string>> result = new List<Tuple<string, string>>();
             StringBuilder stringBuilder = new StringBuilder();
@@ -125,38 +125,74 @@
                 " WHERE c.object_id = OBJECT_ID('{0}') and t.name<>'sysname' ",
                 tableName);
 
+            logWriter.WriteLine("");
+            logWriter.WriteLine(string.Format("> Processing table '{0}'", tableName));
+
             string commandText = stringBuilder.ToString();
             using (SqlCommand schemaCommand = new SqlCommand(commandText) { Connection = sqlConnection })
             {
                 using (SqlDataReader schemaReader = schemaCommand.ExecuteReader())
                 {
+
+                    var processedNames = new HashSet<string>();
+
                     while (schemaReader.Read())
                     {
                         string name = schemaReader.GetString(0);
                         string stringColumnType = schemaReader.GetString(1);
+
+                        if (processedNames.Contains(name))
+                        {
+                            continue;
+                        }
+
+                        processedNames.Add(name);
+
                         switch (stringColumnType)
                         {
                             case "bit":
                                 result.Add(new Tuple<string, string>(name, "boolean"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' is a '{1}' type.", name, "boolean"));
                                 break;
                             case "varchar":
                             case "nvarchar":
                             case "nchar":
+                            case "text":
                             case "char":
                                 result.Add(new Tuple<string, string>(name, "string"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' is a '{1}' type.", name, "sting"));
                                 break;
                             case "int":
                             case "smallint":
                             case "tinyint":
                             case "bigint":
                                 result.Add(new Tuple<string, string>(name, "int"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' is a '{1}' type.", name, "int"));
                                 break;
                             case "float":
+                            case "real":
+                            case "numeric":
+                            case "money":
+                            case "smallmoney":
                             case "decimal":
                                 result.Add(new Tuple<string, string>(name, "float"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' is a '{1}' type.", name, "float"));
                                 break;
                             case "datetime":
+                            case "timestamp":
+                            case "smalldatetime":
+                            case "datetime2":
+                            case "date":
                                 result.Add(new Tuple<string, string>(name, "datetime"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' is a '{1}' type.", name, "datetime"));
+                                break;
+                            case "time":
+                                result.Add(new Tuple<string, string>(name, "time"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' is a '{1}' type.", name, "time"));
+                                break;
+                            default:
+                                result.Add(new Tuple<string, string>(name, "string"));
+                                logWriter.WriteLine(string.Format("    Field name '{0}' did not have a match for type '{1}', setting it to 'string'.", name, stringColumnType));
                                 break;
                         }
                     }

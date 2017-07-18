450cbe9dd9d92ec0a6d95cba7e2e716f130a9ad1
Geert van Horrik <geert@catenalogic.com>
2017-07-18 15:11:38 +0200

    - Remove support for .NET 4.0 - Add support for .NET 4.7 - Update to Catel v5 beta

--- src/Orc.DbToCsv/Orc.DbToCsv.Shared/ModuleInitializer.cs
+++ src/Orc.DbToCsv/Orc.DbToCsv.Shared/ModuleInitializer.cs
@@ -1,7 +1,6 @@
 ﻿
 using Catel.IoC;
 using Catel.Services;
-using Catel.Services.Models;
 
 /// <summary>
 /// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.

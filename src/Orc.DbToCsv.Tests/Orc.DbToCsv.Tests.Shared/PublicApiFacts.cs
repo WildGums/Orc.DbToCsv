c30098bdd187edd9826be15d797bf3eaf44a26d4
Geert van Horrik <geert@catenalogic.com>
2017-10-01 19:17:11 +0200

    Enable ApiApprover to make sure we don't introduce unwanted breaking changes

---
+++ src/Orc.DbToCsv.Tests/Orc.DbToCsv.Tests.Shared/PublicApiFacts.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PublicApiFacts.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.DbToCsv.Tests
+{
+    using ApiApprover;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PublicApiFacts
+    {
+        [Test]
+        public void Orc_DbToCsv_HasNoBreakingChanges()
+        {
+            var assembly = typeof(Importer).Assembly;
+
+            PublicApiApprover.ApprovePublicApi(assembly);
+        }
+    }
+}
